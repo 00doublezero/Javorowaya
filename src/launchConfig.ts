c912139ce9188dd65fb4a99ac8ea818fc1a87a8f
Theodore Tsyganok <ftsiganok@gmail.com>
2019-11-03 01:45:37 +0400

    update launch configs

--- src/launchConfig.ts
+++ src/launchConfig.ts
@@ -3,18 +3,16 @@ export const debugLaunch = {
     headless: false,
     slowMo: 700,
     executablePath: "chromium-browser",
-    userDataDir: "/home/kryger/.config/chromium/",
 };
 export const releaseLaunch = {
     args: ["--start-fullscreen", "--disable-infobars"],
     headless: true,
     executablePath: "chromium-browser",
-    userDataDir: "/home/kryger/.config/chromium/",
 };
 export const proxyDebugLaunch = {
     args: ["--start-fullscreen", "--disable-infobars", "--proxy-server=socks5://127.0.0.1:26000"],
-    // headless: false,
+    headless: false,
     slowMo: 180,
     executablePath: "chromium-browser",
-
+    userDataDir: "hromog-profile",
 };
